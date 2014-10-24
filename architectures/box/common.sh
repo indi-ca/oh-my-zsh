2f1b2d41238d4a4b2f816bb9c762a3ed846099e2
Indika Piyasena <indika.piyasena@netboxblue.com>
2014-10-24 15:56:43 +1000

    password updated

--- architectures/box/common.sh
+++ architectures/box/common.sh
@@ -7,7 +7,7 @@ alias lync_user3_not_work='rdesktop -g800x600 -r clipboard:CLIPBOARD -u user3 -d
 # alias lync_user3='rdesktop -g800x600 -r clipboard:CLIPBOARD -u Administrator -p Oxcoda99 10.3.71.1'
 
 # alias lync_admin='rdesktop -g1920x1160 -r clipboard:CLIPBOARD -u Administrator -d nbbdev2008 -p Oxcoda99 10.12.101.11'
-alias lync_admin='rdesktop -g1920x1160 -r clipboard:CLIPBOARD -u Administrator -d nbbdev2008 -p Oxcoda2015 10.12.101.11'
+alias lync_admin='rdesktop -g1920x1160 -r clipboard:CLIPBOARD -u Administrator -d nbbdev2008 -p Oxcoda201410 10.12.101.11'
 
 # Lync 2010 User on my box
 alias lync_user3='rdesktop -g800x1000 -r clipboard:CLIPBOARD -u user3 -d nbbdev2008 -p Oxcoda99 10.12.10.160'
